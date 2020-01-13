0435ece3c697065e3175a316933e3d50f2cf7405
machi <machi@192.168.0.35>
2020-01-13 22:03:01 +0900

    スキーマ、データファイルの追加

---
+++ src/main/java/org/tomo/dataflow/example/Schema.java
@@ -0,0 +1,16 @@
+package org.tomo.dataflow.example;
+
+import lombok.Data;
+
+/**
+ * スキーマの
+ *
+ * @author machi
+ *
+ */
+@Data
+public class Schema {
+  private String mode;
+  private String name;
+  private String type;
+}
